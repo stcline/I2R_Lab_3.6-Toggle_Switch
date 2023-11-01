63cd4efecc517a8d0e0e95d6ea2a480dc3e3b7e9
Steve Cline <steve.cline@weldre4.org>
2023-11-01 11:13:54 -0600

    Update Starter.ino

--- Starter.ino
+++ Starter.ino
@@ -1,13 +1,32 @@
-int inPin = 7;    // pushbutton connected to digital pin 7
-int val = 0;      // variable to store the read value
+// This sketch is based on Paul McWhorter's tutorial "ARDUINO TUTORIAL 28: USING A PUSHBUTTON AS A TOGGLE SWITCH"
+
+int LEDState=0;    // This variable stores the current state of the LED - start it a off
+int LEDPin=  ;     // The LED pin you are using
+int buttonPin=  ;  // The button pin you are using
+int buttonNew;     // The new button state
+int buttonOld=1;   // The former button state 
+int dt=100;        // Put in a small delay
 
 void setup() {
-  Serial.begin ();
-  pinMode(inPin, );    // sets the digital pin 7 as input
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(LEDPin, );
+pinMode(buttonPin, );
+
 }
 
 void loop() {
-  val = digitalRead(inPin);   // read the input pin
-  // print val as a binary number to the Serial Monitor using Serial.println()
-  // delay 500 ms
+buttonNew=digitalRead();
+if(buttonOld==0 && buttonNew==1){  // This condition is true if the previous button state was off and the new one is on
+  if (LEDState==0){
+    digitalWrite(LEDPin, );  // Turn on the LED
+    LEDState=1;              // Set the current LED state to on
+  }
+  else{
+    digitalWrite(LEDPin, );  // Turn off the LED
+    LEDState=0;
+  }
+}
+buttonOld=buttonNew;
+delay(dt);                    // Slow things down a little bit - try different values here
 }
